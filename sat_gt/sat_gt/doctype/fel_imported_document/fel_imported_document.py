c61489e8f549b4451db7eb9ee1cccd078582ae4c
Rodrigo C. <mistermaster0@gmail.com>
2026-08-14 11:02:31 -0600

    Create fel_imported_document.py

---
+++ sat_gt/sat_gt/doctype/fel_imported_document/fel_imported_document.py
@@ -0,0 +1,9 @@
+"""Controller for imported Guatemala FEL documents."""
+
+from frappe.model.document import Document
+
+
+class FELImportedDocument(Document):
+	"""Persisted representation of an imported FEL XML."""
+
+	pass
